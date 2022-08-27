f4770b7b4d18f7c54ed42da36fc28088e099ea22
Nico-Farias <nicoscs12@gmail.com>
2022-08-27 14:07:16 -0300

    agregue sin stock

--- src/Componets/ItemsProducts/Products.js
+++ src/Componets/ItemsProducts/Products.js
@@ -26,7 +26,16 @@ export default function Product({ id, title, price, image, stock }) {
 
                     <div className="info">
                         <p className="priceP">Precio: $ {price}</p>
-                        <p className="stockP">Disponibles {stock}</p>
+                        <div>
+                            {
+                                stock >= 1 ?
+                                    <p className="stockP"> Disponibles {stock} </p> : <p className="SinStock">Sin stock</p>
+                            }
+
+                        </div>
+
+
+
                     </div>
 
                     <div className="botones">
